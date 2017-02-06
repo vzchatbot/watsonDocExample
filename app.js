4cffedcc45b97670973ebf747fef0ae460d661fc
JosephPravin <josephpravinn@gmail.com>
2017-02-06 21:14:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function convMess(message){
             console.log(err)
         }
         else {
-            console.log("I got a response");
+            console.log('I got a response');
             process.exit(1);
             //console.log('Watson: ' + response.output.text.selection_policy);
             //console.log('Watson: ' + response);
