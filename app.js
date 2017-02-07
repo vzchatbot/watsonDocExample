570c570e8e266794f4dfff12c3d0d0c0f7704a43
JosephPravin <josephpravinn@gmail.com>
2017-02-07 15:25:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-var watson = require('watson-developer-cloud');
-var prompt=require('prompt-sync')();
+// Example 2: adds user input and detects intents.
 
-var conversation = watson.conversation({
-    username: '31be4934-c02e-441a-96e6-d639b4ab69a8',
-    password: 'Q2hapKhopVRj',
-    version: 'v1',
-    version_date: '2016-09-20'
+var prompt = require('prompt-sync')();
+var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+
+// Set up Conversation service wrapper.
+var conversation = new ConversationV1({
+  username: '31be4934-c02e-441a-96e6-d639b4ab69a8', // replace with username from service key
+  password: 'Q2hapKhopVRj', // replace with password from service key
+  path: { workspace_id: 'b2d3a074-4d46-4b95-b902-f70d0000fdc6' }, // replace with workspace ID
+  version_date: '2016-09-20'
 });
 
-function convMess(message) {
-    conversation.message({
-        workspace_id: 'b2d3a074-4d46-4b95-b902-f70d0000fdc6',
-        input: { 'text': message }
-    }, function (err, response) {
-        if (err) {
-            console.log(err)
-        }
-        else {
-            console.log('I got a response. Let me check');
-            if (response.output.text.length != 0) {
-                console.log('Watson says:' + response.output.text[0]);
-            }
-        }
-    });
-}
+// Start conversation with empty message.
+conversation.message({}, processResponse);
 
-//convMess('');
-convMess('Hello');
-var newMessageFromUser = prompt('>> ');
-convMess(newMessageFromUser);
-//convMess('Remind me to meet Robert');
-//convMess('Order me a pizza for lunch');
+// Process the conversation response.
+function processResponse(err, response) {
+  if (err) {
+    console.error(err); // something went wrong
+    return;
+  }
+
+  // If an intent was detected, log it out to the console.
+  if (response.intents.length > 0) {
+    console.log('Detected intent: #' + response.intents[0].intent);
+  }
+
+  // Display the output from dialog, if any.
+  if (response.output.text.length != 0) {
+      console.log(response.output.text[0]);
+  }
+
+  // Prompt for the next round of input.
+  var newMessageFromUser = prompt('>> ');
+  conversation.message({
+    input: { text: newMessageFromUser }
+    }, processResponse)
+}
