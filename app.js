b61c7c596a4a86e56355dff8f76ed5d523c4a5e0
JosephPravin <josephpravinn@gmail.com>
2017-02-07 15:12:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var watson = require('watson-developer-cloud');
-
+var prompt=require('prompt-sync')();
 
 var conversation = watson.conversation({
     username: '31be4934-c02e-441a-96e6-d639b4ab69a8',
@@ -8,33 +8,24 @@ var conversation = watson.conversation({
     version_date: '2016-09-20'
 });
 
-function prompt(question, callback){
-    var stdin=process.stdin,
-        stdout=process.stdout;
-    stdin.resume();
-    stdout.write(question);
-    
-    stdin.once('data', function(data) {
-        callback(data.toString().trim());
-    });
-}
-
-function convMess(message){
+function convMess(message) {
     conversation.message({
-        workspace_id:'b2d3a074-4d46-4b95-b902-f70d0000fdc6',
-        input: {'text':message}
-    }, function(err, response) {
-        if(err) {
+        workspace_id: 'b2d3a074-4d46-4b95-b902-f70d0000fdc6',
+        input: { 'text': message }
+    }, function (err, response) {
+        if (err) {
             console.log(err)
         }
         else {
             console.log('I got a response. Let me check');
-           // process.exit(1);
-            //console.log('Watson: ' + response.output.text.selection_policy);
-            //console.log('Watson: ' + response);
+            if (response.intents.length > 0) {
+                console.log('Detected intent: #' + response.intents[0].intent);
+            }
             if (response.output.text.length != 0) {
-      console.log(response.output.text[0]);
-  }
+                console.log('Watson says:' + response.output.text[0]);
+            }
+            var newMessageFromUser = prompt('>> ');
+            convMess(newMessageFromUser);
         }
     });
 }
